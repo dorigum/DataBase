7644de0d6d7a187000c2faa77364148b9e969ecb
dorigum <41736468+dorigum@users.noreply.github.com>
2021-03-25 21:45:00 +0900

    210325 수정

--- 210324/210324 연습문제2/SQLDB_210324_연습2.sql
+++ 210324/210324 연습문제2/SQLDB_210324_연습2.sql
@@ -13,8 +13,6 @@
         
         
         
-        
-    
     -- 기본키 설정
     ALTER TABLE book
         ADD CONSTRAINT PK_book_bookNo PRIMARY KEY (bookNo);
@@ -56,3 +54,78 @@
 
     ALTER TABLE bookSale
         ADD CONSTRAINT FK_bookSale_client FOREIGN KEY (clientNo) REFERENCES client (clientNo);
+        
+        
+        
+    SELECT * FROM publisher;
+    SELECT * FROM book;
+    SELECT * FROM client;
+    SELECT * FROM bookSale;
+    
+    
+    
+    -- 도서명과 가격만 출력
+    SELECT bookName, bookPrice FROM BOOK;
+    
+    -- 저자만 출력
+    SELECT bookAuthor FROM book;
+    
+    
+    -- 중복 제거 - DISTINCT
+    -- 속성값이 중복되는 것이 있으면 한번만 출력
+     SELECT DISTINCT bookAuthor FROM book;
+     
+     
+    -- 비교 (=, <, >, <=, >=, !=)
+    
+    -- 저자가 '홍길동' 인 도서의 도서명, 저자 출력
+        SELECT bookName, bookAuthor FROM book WHERE bookAuthor = '홍길동';
+    
+    --가격이 30000 이상인 도서의 도서명, 가격, 재고 출력
+        SELECT bookName, bookPrice, bookStock  FROM book WHERE bookPrice >= 30000;
+    
+    -- 재고가 3~5 사이인 도서의 도서명, 재고 출력
+        SELECT bookName, bookStock FROM book WHERE bookStock >= 3 AND bookStock <= 5;
+        
+        
+    -- 범위 ( BETWEEN A AND B)
+        SELECT bookName, bookStock FROM book WHERE bookStock BETWEEN 3 AND 5;
+        
+        
+    -- 리스트에 포함 여부 (IN, NOT IN)
+    
+    -- 출판사 명이 '서울 출판사' (pubNo = '1') 와 '도서출판 강남' (pubNo = '2') 인 도서의
+    -- 도서명, 출판사 번호 출력
+    -- (1)
+        SELECT bookName, pubNo FROM book
+        WHERE pubNo IN ('1', '2');
+    -- (2)
+        SELECT bookName, pubNo FROM book
+        WHERE pubNo = '1' OR pubNo = '2';
+        
+        
+    -- 출판사명이 '도서출판 강남' (pubNo = '2') 이 아닌 도서의
+    -- 도서명, 출판사번호 출력
+        SELECT bookName, pubNo FROM book
+        WHERE pubNo NOT IN ('2');
+        
+        
+        
+        SELECT * FROM client;
+        
+    -- NULL 여부 ( IS NULL, IS NOT NULL);
+        
+    -- 모든 고객명, 취미 출력
+        SELECT clientName, clientHobby FROM client;
+        
+    -- 취미에 NULL 값이 들어 있는 행만 출력
+        SELECT clientName, clientHobby FROM client
+        WHERE clientHobby IS NULL;
+        
+    -- 취미에 NULL 값이 아닌 행만 출력
+        SELECT clientName, clientHobby FROM client
+        WHERE clientHobby IS NOT NULL;
+        
+    -- 취미에 공백이 들어 있는 행만 출력
+        SELECT clientName, clientHobby FROM client
+        WHERE clientHobby = ' ';
